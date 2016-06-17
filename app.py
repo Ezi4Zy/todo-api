941572a0afcc556a66f6caeedbdbfd402bcda0e1
Ezi4zy <Ezi4zy@163.com>
2016-06-17 15:58:34 +0800

    插入一个新的任务

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 # @File:   app.py
 # @Date:   2016-06-17 14:54:40
 
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -30,6 +30,20 @@ def get_tasks():
     return jsonify({'tasks': tasks})
 
 
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
@@ -41,6 +55,11 @@ def get_task(task_id):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
 
 
 if __name__ == '__main__':
