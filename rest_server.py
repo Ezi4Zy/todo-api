c4b105fa730db85e76d7be62cd8fee96e172aa95
Ezi4zy <Ezi4zy@163.com>
2016-06-20 11:13:48 +0800

    修改文件名

--- rest_server.py
+++ rest_server.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Ezi
 # @Email:  Ezi4zy@163.com
-# @File:   app.py
+# @File:   rest_server.py
 # @Date:   2016-06-17 14:54:40
 
 from flask import Flask, jsonify, abort, make_response, request, url_for
